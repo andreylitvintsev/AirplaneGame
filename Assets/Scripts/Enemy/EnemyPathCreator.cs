e66325663501fceee97a82ef36f3341879928127
Andrey Litvintsev <hjrths@yandex.ru>
2020-12-28 22:38:01 +0300

    Add "Enemy" prefab
    - Add Bezier Path Creator package
    - Add "EnemyPathCreator"

---
+++ Assets/Scripts/Enemy/EnemyPathCreator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    [RequireComponent(typeof(PathCreation.PathCreator))]
+    public class EnemyPathCreator : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 FromBounds;
+
+        [SerializeField]
+        private Vector3 ToBounds;
+
+        [SerializeField, Min(2)]
+        private int pointCount = 2;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var bezierPathCreator = GetComponent<PathCreation.PathCreator>().bezierPath;
+            for (int i = 0; i < pointCount; ++i)
+            {
+                bezierPathCreator.AddSegmentToEnd(RandomPoint());
+            }
+        
+            // удаляем первоначально сгенерированные библиотекой сегменты
+            bezierPathCreator.DeleteSegment(0);
+            bezierPathCreator.DeleteSegment(1);
+
+            bezierPathCreator.IsClosed = true;
+        }
+
+        private Vector3 RandomPoint()
+        {
+            return new Vector3(
+                Random.Range(FromBounds.x, ToBounds.x),
+                Random.Range(FromBounds.y, ToBounds.y),
+                Random.Range(FromBounds.z, ToBounds.z)
+            );
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
