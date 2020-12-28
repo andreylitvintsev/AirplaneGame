aa7d8cb90966d80d723aa76f10fa2e721509640f
Andrey Litvintsev <hjrths@yandex.ru>
2020-12-28 21:41:22 +0300

    Add Player scripts

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,60 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        [SerializeField] private PlayerController _playerController = null;
+        [SerializeField, Min(0f)] private float _longitudinalAngleBound = 0f;
+        [SerializeField, Min(0f)] private float _followingDistance = 0f;
+
+        private void AfterPlayerInputHandled(float verticalInput, float horizontalInput)
+        {
+            var playerTransform = _playerController.transform;
+            var newCameraPosition = playerTransform.TransformPoint(
+                Vector3.back.RotateByEulerX(-verticalInput * _longitudinalAngleBound) * _followingDistance);
+            transform.position = newCameraPosition;
+            
+            // TODO: расчитать up вектор (отложить)
+            transform.LookAt(playerTransform, playerTransform.up);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            var playerTransform = _playerController.transform;
+            var playerPosition = playerTransform.position;
+            
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(transform.position, playerPosition);
+                
+            var upAngleBoundVector = playerTransform.TransformPoint(
+                Vector3.back.RotateByEulerX(_longitudinalAngleBound) * _followingDistance);
+            var downAngleBoundVector = playerTransform.TransformPoint(
+                Vector3.back.RotateByEulerX(-_longitudinalAngleBound) * _followingDistance);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(playerPosition, upAngleBoundVector);
+            Gizmos.DrawLine(playerPosition, downAngleBoundVector);
+        }
+
+        private void OnEnable()
+        {
+            if (_playerController != null)
+            {
+                _playerController.AfterInputHandled += AfterPlayerInputHandled;
+            }
+            else
+            {
+                Debug.LogError("Player Controller is null", this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_playerController != null)
+            {
+                _playerController.AfterInputHandled -= AfterPlayerInputHandled;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Speed")]
+        [SerializeField, Min(0f)] private float _longitudinalRollSpeed = 0f;
+        [SerializeField, Min(0f)] private float _lateralRollSpeed = 0f;
+        [SerializeField, Min(0f)] private float _speed = 0f;
+
+        [Header("Control")]
+        [SerializeField] private bool _invertHorizontal = false;
+        [SerializeField] private bool _invertVertical = false;
+
+        public event Action<float, float> AfterInputHandled;
+
+        private void Update()
+        {
+            var deltaTime = Time.deltaTime;
+            var invertHorizontal = _invertHorizontal ? -1 : 1;
+            var invertVertical = _invertVertical ? -1 : 1;
+            var verticalInput = Input.GetAxis("Vertical");
+            var horizontalInput = Input.GetAxis("Horizontal");
+            var rotateAngles = new Vector3(
+                invertVertical * _longitudinalRollSpeed * verticalInput * deltaTime,
+                0f,
+                invertHorizontal * _lateralRollSpeed * horizontalInput * deltaTime
+            );
+            var cachedTransform = transform;
+            cachedTransform.Rotate(rotateAngles);
+            cachedTransform.position += cachedTransform.forward * _speed * deltaTime;
+        
+            AfterInputHandled?.Invoke(verticalInput, horizontalInput);
+        }
+    }
+}
